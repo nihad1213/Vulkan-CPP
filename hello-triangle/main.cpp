7cff712f2c0c69a7e708844025e72e4c485f482b
nihad1213 <nihad.nemetli@gmail.com>
2025-09-14 22:55:06 +0400

    createInstance method created

--- hello-triangle/main.cpp
+++ hello-triangle/main.cpp
@@ -21,6 +21,17 @@ class HelloTriangleApplication {
     private:
 
         GLFWwindow* window; 
+        VkInstance instance;
+
+        void createInstance() {
+            VkApplicationInfo appInfo{};
+            appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+            appInfo.pApplicationName = "Hello Triangle";
+            appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+            appInfo.pEngineName = "No Engine";
+            appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+            appInfo.apiVersion = VK_API_VERSION_1_0;
+        }
 
         void initWindow() {
             glfwInit();
@@ -30,7 +41,7 @@ class HelloTriangleApplication {
         }
 
         void initVulkan() {
-            // Initialization code for Vulkan would go here
+            createInstance();
         }
 
         void mainLoop() {
