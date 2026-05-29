d2943776b6beb983c07aa4b8e80deacc0733708f
Nihad <nihad.nemetli@gmail.com>
2026-05-29 20:53:30 +0400

    new: Constructor and destructor created

--- 01.creating-window/vulkan_engine.hpp
+++ 01.creating-window/vulkan_engine.hpp
@@ -1,10 +1,22 @@
 #pragma once
 
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <string>
 
 namespace ve {
     class VulkanEngine {
+
+        public:
+            VulkanEngine(int w, int h, std::string name);
+            ~VulkanEngine();
+
         private:
             GLFWwindow* window;
+
+            void initWindow();
+
+            const int width;
+            const int height;
     };
 }
